ad2b5e02b17ee7bffadc54df47ab092d8435e63e
tsao <tsaoyuan@gmail.com>
2022-06-11 14:00:15 +0800

    feat: 建立 create 方法
    - 透過 params[:friend] 的 params method 可以取得前一個畫面拋過來那些欄位的資
    料,並且把它傳給 Friend Model 的 new 方法以建立物件。
    - if...else 判斷成功/失敗 的回饋
    - Strong Parameters 問題, 存入 Friend Model 的資料未被清洗或過濾 => 錯誤

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,4 +6,18 @@ class FriendsController < ApplicationController
   def new
     @friend = Friend.new
   end
+
+  def create
+    @friend = Friend.new(params[:friend])
+    # 成功
+    if @friend.save
+      redirect_to friends_path, notice: "新增朋友成功!"
+    # 失敗
+    else 
+      render :new
+    end  
+
+  end
+
+  
 end
