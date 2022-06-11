97e961f4edd05b2ae83a081e2a4dddd5c109f418
tsao <tsaoyuan@gmail.com>
2022-06-11 16:37:22 +0800

    feat: 在朋友列表中, 設置 編輯; 新增 編輯的頁面
    - 編輯/新增的頁面內容相同, 可利用 局部渲染, 減少重複撰寫次數 (DRY)
    - edit action is responsible for rendering the view
    - update action is responsible for interacting with the model (db updates etc)
    ----------------
    ps.
    - New is the precursor action to render a form, that upon submitting, runs the Create action.
    - Edit is the precursor action to render a form, that upon submitting, runs the Update action.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -18,6 +18,10 @@ class FriendsController < ApplicationController
     end  
   end
 
+  def edit
+    @friend = Friend.find_by(id: params[:id])
+  end
+
   private
     def friend_params
       params.require(:friend).permit(:name, :email, :tel, :twitter)
